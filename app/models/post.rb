da1ca5d088ba7cc859dbbae98fc6de9f565ba301
mengshalitsofi <mengshalitsofi@gmail.com>
2021-03-15 13:00:38 -0700

    commit tree 370de6752e2439c9c19ad32c07e30a645ad5c8dd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates(:category, inclusion: ["Fiction", "Non-Fiction"])
+    validate :clickbait
+
+    def clickbait
+        if (title =~ /Won't Believe|Secret|Guess|Top [0-9]+/) == nil
+            errors.add(:title, "Not clickbait-y enough")
+        end
+    end
 end
